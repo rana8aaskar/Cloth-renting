10ae5ee91e52506f5452cae1862e4237da30735b
Aaskar Rana <aaskar04rana@gmail.com>
2025-04-01 23:41:28 +0530

    implement SignIn form with state management, error handling, and loading state; enhance UI with form inputs and navigation link

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
 
-function SignIn() {
+
+import React, { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+
+export default function SignIn() {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  
+    const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      })
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      try {
+        setLoading(true)
+        const res = await fetch("/server/auth/signin",
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          }
+        ) 
+        const data = await res.json()
+        if(data.success === false) {
+          setError(data.message)
+          setLoading(false)
+          return
+        }
+        setLoading(false)
+        setError(null)
+        navigate("/")
+      } catch (error) {
+        set.loading(false)
+        setError("Something went wrong")
+      }
+
+      
+      
+    }
+    
+    
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+       
+        <input
+          type="email"
+          placeholder="Email"
+          className="bg-white border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-white border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          id="password"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className='bg-slate-700 text-white  p-3  rounded-lg  uppercase  hover:opacity-95 disabled:opacity-80'>
+          {loading? 'loadiing...':'Sign Up' }</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link  to ={"/sign-up"}>
+        <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 text-center mt-4'>{error}</p>}
+    </div>
+  );
 }
 
-export default SignIn
+
