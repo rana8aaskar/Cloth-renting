50ed28d178dce3aeffe818d1e1e32b359ae8e72b
Aaskar Rana <aaskar04rana@gmail.com>
2025-07-22 18:34:52 +0530

    feat: add build script to package.json for client-side dependencies

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ import listingRouter from "./routes/listing.route.js"
 import cors from 'cors';
 import uploadRouter from "./routes/upload.route.js"
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 
 
 dotenv.config({
@@ -41,7 +43,11 @@ app.use("/server/auth", authRouter)
 app.use("/server/upload", uploadRouter)
 app.use("/server/listing",listingRouter)
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
